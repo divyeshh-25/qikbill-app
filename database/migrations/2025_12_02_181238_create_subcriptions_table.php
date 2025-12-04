87c5609ef3f90b8e561e4dab566e8b539e3f41af
divyeshh-25 <info.divyeshpanchal@gmail.com>
2025-12-04 22:51:36 +0530

    DB Changes

--- database/migrations/2025_12_02_181238_create_subcriptions_table.php
+++ database/migrations/2025_12_02_181238_create_subcriptions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tenant_plans', function (Blueprint $table) {
+        Schema::create('subcriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('tenant_id')->constrained()->onDelete('cascade');
             $table->foreignId('plan_id')->constrained()->onDelete('cascade');
